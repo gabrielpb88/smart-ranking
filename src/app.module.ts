d8e70683d142e8dfe789e0ecf9b79874ed7da77d
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-05-02 23:18:54 -0300

    feat: add client and config for rabbitmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot(), ProxyRMQModule]
 })
 export class AppModule {}
