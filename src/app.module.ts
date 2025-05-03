c4e9e2dcc7bab1dfa9b77c4fe069f718061dddde
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-05-02 23:45:48 -0300

    feat: add config for mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ 
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        user: configService.get<string>('MONGODB_USER'),
+        pass: configService.get<string>('MONGODB_PASS'),
+        authSource: configService.get<string>('MONGODB_AUTH_SOURCE')
+      }),
+      inject: [ConfigService],
+    })],
   controllers: [],
   providers: [],
 })
