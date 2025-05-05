15e98d767dbd81268fc83c9fae7d8c2b654277cc
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-05-05 17:36:32 -0300

    feat: add update category resource

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Get, Body, Query, ValidationPipe, UsePipes } from '@nestjs/common';
+import { Controller, Post, Get, Put, Body, Query, Param, ValidationPipe, UsePipes } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { ClientProxySmartRanking } from 'src/proxyrmq/client-proxy';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('api/v1/category')
 export class CategoryController {
@@ -22,6 +23,16 @@ export class CategoryController {
   @Get()
   findAll(@Query('idCategory') _id: string): Observable<any> {
     return this.clientProxy.send('find-category', _id ? _id : '')
+  }
+
+  @Put('/:_id')
+  @UsePipes(ValidationPipe)
+  updateCategory(
+    @Body() updateCategoryDto: UpdateCategoryDto,
+    @Param('_id') _id: string
+  ) {
+    const category = updateCategoryDto
+    return this.clientProxy.emit('update-category', { _id, category })
   }
 
 }
