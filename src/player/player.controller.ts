aff9e8f9ceb551003e02d6dca0a0030dfaf86ee6
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-05-14 16:21:33 -0300

    feat: add find player resource

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Logger } from '@nestjs/common';
-import { EventPattern, Payload, Ctx, RmqContext } from '@nestjs/microservices';
+import { BadRequestException, Controller, Logger } from '@nestjs/common';
+import { EventPattern, Payload, Ctx, RmqContext, MessagePattern, RpcException } from '@nestjs/microservices';
 import { PlayerService } from './player.service';
 import { CreatePlayerDto } from './dto/create-player.dto';
+import { Player } from './interface/player.interface';
 
 @Controller()
 export class PlayerController {
@@ -23,5 +24,41 @@ export class PlayerController {
       channel.nack(originalMsg, false, false)
     }
 
+  }
+
+  @EventPattern('update-player')
+  async updatePlayer(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef()
+    const originalMsg = context.getMessage()
+
+    try {
+      const _id: string = data.id
+      const player: Player = data.player
+      await this.playerService.updatePlayer(_id, player);
+      await channel.ack(originalMsg)
+    } catch (error) { 
+      this.logger.error(`error: ${JSON.stringify(error)}`)
+      await channel.nack(originalMsg, false, false)
+    }
+  }
+
+  @MessagePattern('find-player')
+  async findPlayers(@Payload() _id: string, @Ctx() context: RmqContext): Promise<Player[] | Player | null> {
+    const channel = context.getChannelRef()
+    const originalMsg = context.getMessage()
+
+    let result
+    try {
+      if(_id) {
+        result = await this.playerService.findPlayerById(_id);
+      } else {
+        result = await this.playerService.findAllPlayers();
+      }
+      await channel.ack(originalMsg)
+      return result;
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error)}`)
+      await channel.nack(originalMsg, false, false)
+    }
   }
 }
--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -3,6 +3,8 @@ import { CreatePlayerDto } from './dto/create-player.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Player } from './interface/player.interface';
+import { RpcException } from '@nestjs/microservices';
+import { isMongoId, validate } from 'class-validator';
 
 @Injectable()
 export class PlayerService {
@@ -18,7 +20,39 @@ export class PlayerService {
     }
     catch (error) {
       this.logger.error(`Error creating player: ${JSON.stringify(error.message)}`);
-      throw new Error('Error creating player');
+      throw new RpcException('Error creating player');
+    }
+  }
+
+  async updatePlayer(_id: string, player: Player): Promise<void> {
+    try {
+      if(!isMongoId(_id)) {
+        throw new RpcException(`Invalid Id`)
+      }
+      await this.playerModel.findByIdAndUpdate({ _id }, { $set: player }).exec()    
+    } catch (error) {
+      this.logger.error(`Error updating player: ${JSON.stringify(error.message)}`)
+      throw new RpcException(error.message)
+    }
+  }
+
+  async findPlayerById(_id: string): Promise<Player | null | undefined> {
+    if(!isMongoId(_id)) {
+      return null
+    }
+    try {
+      return this.playerModel.findById(_id).exec()
+    } catch (error) {
+      this.logger.error(`Error: ${JSON.stringify(error.message)}`)
+    }
+  }
+
+  async findAllPlayers(): Promise<Player[]> {
+    try {
+      return this.playerModel.find().exec()
+    } catch (error) {
+      this.logger.error(`Error: ${JSON.stringify(error.message)}`)
+      return []
     }
   }
 }
