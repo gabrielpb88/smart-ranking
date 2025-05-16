fcdebbf5903aceb25dde5fcc1a5391d0e5e0276b
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-05-15 22:46:51 -0300

    feat: add delete player resource

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Body, Controller, Logger, Get, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Logger, Get, Post, Query, UsePipes, ValidationPipe, Delete, Put, Param } from '@nestjs/common';
 import { ClientProxySmartRanking } from 'src/proxyrmq/client-proxy';
 import { CreatePlayerDto } from './dto/create-player.dto';
+import { UpdatePlayerDto } from './dto/update-player.dto';
 
 @Controller('api/v1/player')
 export class PlayerController {
@@ -33,5 +34,21 @@ export class PlayerController {
             this.logger.error(`Error finding player: ${JSON.stringify(error)}`);
             throw new BadRequestException(error.message);
         }
+    }
+
+    @Put('/:_id')
+    @UsePipes(ValidationPipe)
+    async updatePlayer(@Param('_id') _id: string, @Body() updatePlayerDto: UpdatePlayerDto) {
+        const category = await this.clientProxy.send('find-category', updatePlayerDto.category).toPromise()
+        if(category) {
+            await this.clientProxy.send('update-player', { id: _id, player: updatePlayerDto })
+        } else {
+            throw new BadRequestException('Category not found');
+        }
+    }
+
+    @Delete('/:_id')
+    async deletePlayers(@Param('_id') _id: string) {
+        await this.clientProxy.emit('delete-player', _id)
     }
 }
