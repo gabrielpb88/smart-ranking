6d5b28f15c370dfbb0485e1f463b6d16feead1eb
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-05-19 16:36:06 -0300

    refactor: optimize imports

--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreatePlayerDto } from './dto/create-player.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
