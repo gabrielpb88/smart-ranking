a01458dee735ad9643f75e38ce559e2e46602f14
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-05-19 16:18:25 -0300

    fix: added CategoryModel to PlayerModule

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -3,9 +3,14 @@ import { PlayerService } from './player.service';
 import { PlayerController } from './player.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayerSchema } from './entities/player.entity';
+import { CategorySchema } from 'src/category/entities/category.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Player', schema: PlayerSchema },
+      { name: 'Category', schema: CategorySchema }]),
+  ],
   controllers: [PlayerController],
   providers: [PlayerService],
 })
